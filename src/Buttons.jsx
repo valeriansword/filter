8d5e3eaf616471680c5c0e196fcc0fc67f7a6bc6
valeriansword <112816779+valeriansword@users.noreply.github.com>
2024-02-28 20:16:14 +0530

    Update Buttons.jsx

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Data from "./Data";
 function Buttons(props){
     return(
-    <div className="d-flex justify-content-center">
+    <div className=" row d-flex justify-content-center">
         {
             props.menuItems.map((val)=>(
-                <button className="btn btn-dark text-white fw-bold p-1 mb-4 mx-5"
+                <button className="col-md-4 col-sm-3 btn btn-dark text-white fw-bold p-1 mb-4 mx-5"
                 onClick={()=>{props.filterItems(val)}}>
                     {val}
                 </button>
             ))
         }
-       <button className="btn btn-dark text-white fw-bold p-1 mb-4 px-2 mx-5"
+       <button className="col-md-4 col-sm-3 btn btn-dark text-white fw-bold p-1 mb-4 px-2 mx-5"
        onClick={()=>{props.setItem(Data)}}>
                     All
         </button>
